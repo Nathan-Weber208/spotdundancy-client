19e029134e11d5f624ddf904198a1041f22e932f
Nathan-Weber208 <87921702+Nathan-Weber208@users.noreply.github.com>
2023-12-07 17:23:43 -0700

    style

--- popup.js
+++ popup.js
@@ -1,16 +1,28 @@
 // popup.js
-document.addEventListener("DOMContentLoaded", function () {
-  const urlElement = document.getElementById("url");
+document.addEventListener("DOMContentLoaded", async function () {
+  // Clear local storage
+  // localStorage.clear();
+  const playlistNameElement = document.getElementById("playlistName");
   const closeButton = document.getElementById("closeButton");
   const DownloadMP3Button = document.getElementById("DownloadMP3Button");
   const DownloadCSVButton = document.getElementById("DownloadCSVButton");
   const SeePlaylistButton = document.getElementById("SeePlaylistButton");
+  // call getPlaylistDetails() and make sure it's done before continuing
+  await getPlaylistDetails();
 
-  // Get the current URL and display it in the popup
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    const url = tabs[0].url;
-    urlElement.textContent = url;
-  });
+
+
+  // get playlistName and tracks from local storage
+  const playlistName = localStorage.getItem('playlistName');
+  const tracks = localStorage.getItem('tracks');
+  console.log('Playlist Name:', playlistName);
+  console.log('Tracks:', tracks);
+  // put the playlist title in the container playlist-title
+  playlistNameElement.innerHTML = playlistName;
+  // put the number of tracks in the container numTracks with the words "Tracks in playlist"
+  // calculate the number of tracks in the playlist by unstringifying the tracks and getting the length
+  const numTracks = JSON.parse(tracks).length;
+  document.getElementById('numTracks').innerHTML = `${numTracks} Tracks in playlist`;
 
   // Close button
   closeButton.addEventListener("click", () => {
@@ -19,29 +31,54 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Download CSV button
   DownloadCSVButton.addEventListener("click", async () => {
-    const playlistData = await getPlaylistDetails();
-    savePlaylistToCSV(playlistData.playlistName, playlistData.tracks);
+    // pull from local storage
+    const playlistName = localStorage.getItem('playlistName');
+    // unstringify the tracks
+    const tracks = JSON.parse(localStorage.getItem('tracks'));
+    // call savePlaylistToCSV()
+    await savePlaylistToCSV(playlistName, tracks);
   });
 
   // Download MP3 button
   DownloadMP3Button.addEventListener("click", async () => {
-    const playlistData = await getPlaylistDetails();
-    console.log("playlistData:")
-    console.log(playlistData);
+    const playlistName = localStorage.getItem('playlistName');
+    const tracks = localStorage.getItem('tracks');
+    console.log({ playlistName, tracks });
 
     console.log("JSON:")
-    console.log(JSON.stringify(playlistData));
+    // stringify both playlistName and tracks in one JSON object
+    console.log(JSON.stringify({ playlistName, tracks }));
   });
+
 
   // See Playlist button
   SeePlaylistButton.addEventListener("click", async () => {
-    const playlistData = await getPlaylistDetails();
 
-    chrome.storage.local.set({ 'playlistData': playlistData }, function () {
-      console.log('Playlist data saved');
-      chrome.tabs.create({ url: 'viewingportal.html' });
-    });
+    chrome.tabs.create({ url: 'viewingportal.html' });
+
   });
+
+  // check the url of the current tab if it doesnt start with https://open.spotify.com/playlist/ then hide all buttons except for the close button
+  // also add a message to the user saying "Please navigate to a Spotify playlist to use this extension"
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var currentTab = tabs[0];
+    if (!currentTab.url.startsWith('https://open.spotify.com/playlist/')) {
+        // Hide all buttons except for the close button
+        document.querySelectorAll('button:not(#closeButton)').forEach(button => {
+            button.style.display = 'none';
+        });
+        // remove the playlist title and number of tracks
+        document.getElementById('playlistName').style.display = 'none';
+        document.getElementById('numTracks').style.display = 'none';
+        // Show a message to the user
+        var messageElement = document.createElement('p');
+        messageElement.textContent = 'Please navigate to a Spotify playlist to use this extension';
+        document.body.appendChild(messageElement);
+    }
+});
+    
+
+
 
 
 });
@@ -120,74 +157,75 @@ async function getAccessToken(clientId, clientSecret) {
 }
 
 async function getPlaylistDetails() {
-  const urlElement = document.getElementById('url');
   try {
-    // Get the URL from the urlElement
-    const playlistUrl = urlElement.textContent;
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      const playlistUrl = tabs[0].url;
 
-    // Extract the playlist ID from the URL if provided
-    const playlistIdMatch = playlistUrl.match(/playlist\/(\w+)/);
-    if (!playlistIdMatch) {
-      throw new Error('Invalid playlist URL');
-    }
-    const playlistId = playlistIdMatch[1];
+      // Extract the playlist ID from the URL if provided
+      const playlistIdMatch = playlistUrl.match(/playlist\/(\w+)/);
+      if (!playlistIdMatch) {
+        throw new Error('Invalid playlist URL');
+      }
+      const playlistId = playlistIdMatch[1];
 
-    //  JACOB'S CLIENT ID AND SECRET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    const clientId = '6c07df458f764d6eba746f4f3d282efc';
-    const clientSecret = '13e852c924834ab9a87c2e4d9f4c3215';
+      //  JACOB'S CLIENT ID AND SECRET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      const clientId = '6c07df458f764d6eba746f4f3d282efc';
+      const clientSecret = '13e852c924834ab9a87c2e4d9f4c3215';
 
-    // Get the access token
-    const accessToken = await getAccessToken(clientId, clientSecret);
-    if (!accessToken) {
-      throw new Error('Failed to get access token');
-    }
-
-    // Spotify API endpoint for getting playlist details
-    const playlistEndpoint = `https://api.spotify.com/v1/playlists/${playlistId}`;
-
-    // Set up headers with the access token
-    const headers = {
-      'Authorization': `Bearer ${accessToken}`,
-    };
-
-    // Fetch the playlist details
-    const response = await fetch(playlistEndpoint, {
-      method: 'GET',
-      headers: headers,
-    });
-
-    if (response.ok) {
-      const playlistData = await response.json();
-
-      if (!playlistData) {
-        console.error('Playlist data is null or undefined');
-        return null;
+      // Get the access token
+      const accessToken = await getAccessToken(clientId, clientSecret);
+      if (!accessToken) {
+        throw new Error('Failed to get access token');
       }
 
-      const playlistName = playlistData.name;
+      // Spotify API endpoint for getting playlist details
+      const playlistEndpoint = `https://api.spotify.com/v1/playlists/${playlistId}`;
 
-      // Extract track details
-      const tracks = playlistData.tracks.items.map(item => {
-        const track = item.track;
-        const artists = track.artists.map(artist => artist.name);
+      // Set up headers with the access token
+      const headers = {
+        'Authorization': `Bearer ${accessToken}`,
+      };
 
-        return {
-          title: track.name,
-          artist: artists.join(', '),
-          album: track.album.name,
-          coverArtUrl: track.album.images[0].url,
-          releaseDate: track.album.release_date,
-          // Add more metadata fields here as needed
-        };
+      // Fetch the playlist details
+      const response = await fetch(playlistEndpoint, {
+        method: 'GET',
+        headers: headers,
       });
 
-      return { playlistName, tracks };
-    } else {
-      console.error('Failed to fetch playlist details:', response.statusText);
-      return null;
-    }
+      if (response.ok) {
+        const playlistData = await response.json();
+
+        if (!playlistData) {
+          console.error('Playlist data is null or undefined');
+          return null;
+        }
+
+        const playlistName = playlistData.name;
+
+        // Extract track details
+        const tracks = playlistData.tracks.items.map(item => {
+          const track = item.track;
+          const artists = track.artists.map(artist => artist.name);
+
+          return {
+            title: track.name,
+            artist: artists.join(', '),
+            album: track.album.name,
+            coverArtUrl: track.album.images[0].url,
+            releaseDate: track.album.release_date,
+            // Add more metadata fields here as needed
+          };
+        });
+        // Store playlistName and tracks in local storage
+        localStorage.setItem('playlistName', playlistName);
+
+        // Store tracks in local storage stringified
+        localStorage.setItem('tracks', JSON.stringify(tracks));
+      } else {
+        console.error('Failed to fetch playlist details:', response.statusText);
+      }
+    });
   } catch (error) {
     console.error('Error while getting playlist details:', error);
-    return null;
   }
 }
--- viewingportal.js
+++ viewingportal.js
@@ -1,32 +1,28 @@
 jQuery(document).ready(function () {
 
-    chrome.storage.local.get('playlistData', function (data) {
-        const playlistData = data.playlistData;
-        console.log(playlistData);
-        const playlistContainer = document.getElementById('playlist-container');
-        let playlistHTML = '<ul>';
-        // put the playlist title in the container playlist-title
-        document.getElementById('playlist-title').innerHTML = playlistData.playlistName;
+    const playlistName = localStorage.getItem('playlistName');
+    const tracks = JSON.parse(localStorage.getItem('tracks'));
+    console.log('Playlist Name:', playlistName);
+    console.log('Tracks:', tracks);
+    const playlistContainer = document.getElementById('playlist-container');
+    let playlistHTML = '<ul>';
+    // put the playlist title in the container playlist-title
+    document.getElementById('playlist-title').innerHTML = playlistName;
 
-        playlistData.tracks.forEach(track => {
-            playlistHTML += `
-            <li>
-                <img src="${track.coverArtUrl}" alt="Album Cover">
-                <div class="track-details">
-                    <h3>${track.title}</h3>
-                    <p>Artist: ${track.artist}</p>
-                    <p>Album: ${track.album}</p>
-                    <p>Release Date: ${track.releaseDate}</p>
-                </div>
-            </li>`;
-        });
-
-        playlistHTML += '</ul>';
-        playlistContainer.innerHTML = playlistHTML;
+    tracks.forEach(track => {
+        playlistHTML += `
+        <li>
+            <img src="${track.coverArtUrl}" alt="Album Cover">
+            <div class="track-details">
+                <h3>${track.title}</h3>
+                <p>Artist: ${track.artist}</p>
+                <p>Album: ${track.album}</p>
+                <p>Release Date: ${track.releaseDate}</p>
+            </div>
+        </li>`;
     });
 
+    playlistHTML += '</ul>';
+    playlistContainer.innerHTML = playlistHTML;
 
 });
-
-
-
