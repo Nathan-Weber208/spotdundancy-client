ec16a05eb5cb9b415a78a57448d0fb547ede5cc2
Nathan-Weber208 <87921702+Nathan-Weber208@users.noreply.github.com>
2023-12-07 20:12:28 -0700

    got the youtube done and looking good

--- popup.js
+++ popup.js
@@ -1,14 +1,27 @@
 // popup.js
+
 document.addEventListener("DOMContentLoaded", async function () {
-  // Clear local storage
-  // localStorage.clear();
+
   const playlistNameElement = document.getElementById("playlistName");
   const closeButton = document.getElementById("closeButton");
   const DownloadMP3Button = document.getElementById("DownloadMP3Button");
   const DownloadCSVButton = document.getElementById("DownloadCSVButton");
   const SeePlaylistButton = document.getElementById("SeePlaylistButton");
-  // call getPlaylistDetails() and make sure it's done before continuing
-  await getPlaylistDetails();
+
+  verifyToken().then(async () => {
+    await getPlaylistDetails();
+  });
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -60,28 +73,132 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   // check the url of the current tab if it doesnt start with https://open.spotify.com/playlist/ then hide all buttons except for the close button
   // also add a message to the user saying "Please navigate to a Spotify playlist to use this extension"
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     var currentTab = tabs[0];
     if (!currentTab.url.startsWith('https://open.spotify.com/playlist/')) {
-        // Hide all buttons except for the close button
-        document.querySelectorAll('button:not(#closeButton)').forEach(button => {
-            button.style.display = 'none';
-        });
-        // remove the playlist title and number of tracks
-        document.getElementById('playlistName').style.display = 'none';
-        document.getElementById('numTracks').style.display = 'none';
-        // Show a message to the user
-        var messageElement = document.createElement('p');
-        messageElement.textContent = 'Please navigate to a Spotify playlist to use this extension';
-        document.body.appendChild(messageElement);
+      // Hide all buttons except for the close button
+      document.querySelectorAll('button:not(#closeButton)').forEach(button => {
+        button.style.display = 'none';
+      });
+      // remove the playlist title and number of tracks
+      document.getElementById('playlistName').style.display = 'none';
+      document.getElementById('numTracks').style.display = 'none';
+      // Show a message to the user
+      var messageElement = document.createElement('p');
+      messageElement.textContent = 'Please navigate to a Spotify playlist to use this extension';
+      document.body.appendChild(messageElement);
     }
+  });
+
+
+  // test the youtube search
+  const searchQuery = 'Boston, Augustana, All The Stars and Boulevards, 2005';
+  const youtubeLink = getFirstNonSponsoredVideoLink(searchQuery);
+  console.log(youtubeLink);
+
+
+
+
+
 });
-    
+
+
+
+async function getFirstNonSponsoredVideoLink(searchQuery) {
+  const formattedQuery = searchQuery.replace(/\s+/g, '+');
+  const apiKey = 'AIzaSyB7q6DDaMMxEnNvjUL0QlW_MtTeZymonEU';
+  const searchUrl = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=${formattedQuery}&key=${apiKey}`;
+
+  try {
+    // Fetch the search results from the YouTube Data API
+    const response = await fetch(searchUrl);
+    const data = await response.json();
+
+    // Check if a video was found
+    if (data.items && data.items.length > 0) {
+      const videoId = data.items[0].id.videoId;
+
+      // Construct the YouTube video link
+      const youtubeLink = `https://www.youtube.com/watch?v=${videoId}`;
+      // console.log('YouTube Link:', youtubeLink);
+
+      return youtubeLink;
+    } else {
+      throw new Error('No video found.');
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+    return null;
+  }
+}
 
 
 
 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function verifyToken() {
+  return new Promise((resolve, reject) => {
+    let accessToken = localStorage.getItem('accessToken');
+    if (!accessToken || accessToken === 'null') {
+      // Hide all elements
+      document.body.innerHTML = '';
+
+      // Show form
+      document.body.innerHTML = `
+              <form id="credentialsForm">
+                  <label for="clientId">Enter your Spotify Client ID:</label>
+                  <input type="text" id="clientId" name="clientId" required>
+
+                  <label for="clientSecret"> Enter your Spotify Client Secret:</label>
+                  <input type="text" id="clientSecret" name="clientSecret" required>
+
+                  <button id="saveButton">Save</button>
+              </form>
+          `;
+
+      // Attach event listener to Save button
+      document.getElementById('saveButton').addEventListener('click', function (e) {
+        e.preventDefault();
+        let clientId = document.getElementById('clientId').value;
+        let clientSecret = document.getElementById('clientSecret').value;
+        getAccessToken(clientId, clientSecret).then(accessToken => {
+          localStorage.setItem('accessToken', accessToken);
+          location.reload(); // Refresh the popup
+          resolve();
+        });
+      });
+    } else {
+      resolve();
+    }
+  });
+}
+
+
+
+
+
+
+
+
 
 async function savePlaylistToCSV(playlistName, playlistDetails) {
   try {
@@ -168,15 +285,8 @@ async function getPlaylistDetails() {
       }
       const playlistId = playlistIdMatch[1];
 
-      //  JACOB'S CLIENT ID AND SECRET!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-      const clientId = '6c07df458f764d6eba746f4f3d282efc';
-      const clientSecret = '13e852c924834ab9a87c2e4d9f4c3215';
-
-      // Get the access token
-      const accessToken = await getAccessToken(clientId, clientSecret);
-      if (!accessToken) {
-        throw new Error('Failed to get access token');
-      }
+      // Get the access token from local storage
+      const accessToken = localStorage.getItem('accessToken');
 
       // Spotify API endpoint for getting playlist details
       const playlistEndpoint = `https://api.spotify.com/v1/playlists/${playlistId}`;
@@ -203,19 +313,30 @@ async function getPlaylistDetails() {
         const playlistName = playlistData.name;
 
         // Extract track details
-        const tracks = playlistData.tracks.items.map(item => {
+        const trackPromises = playlistData.tracks.items.map(async item => {
           const track = item.track;
-          const artists = track.artists.map(artist => artist.name);
-
+          const title = track.name;
+          const artist = track.artists[0].name;
+          const album = track.album.name;
+          const coverArtUrl = track.album.images[0].url;
+          const releaseDate = track.album.release_date;
+        
+          const youtubeLinkObject = await getFirstNonSponsoredVideoLink(`${track.name}, ${track.artists.join}, ${track.album.name}, ${track.album.release_date}`);
+          const youtubeLink = youtubeLinkObject ? youtubeLinkObject : 'No video found';
+        
           return {
-            title: track.name,
-            artist: artists.join(', '),
-            album: track.album.name,
-            coverArtUrl: track.album.images[0].url,
-            releaseDate: track.album.release_date,
-            // Add more metadata fields here as needed
+            title,
+            artist,
+            album,
+            coverArtUrl,
+            releaseDate,
+            youtubeLink,
           };
         });
+
+        const tracks = await Promise.all(trackPromises);
+
+
         // Store playlistName and tracks in local storage
         localStorage.setItem('playlistName', playlistName);
 
